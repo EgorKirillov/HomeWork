75bc8fcc6a7f6e7300e75130c5ca61d21e2de142
EgorKirillov <80296253508@mail.ru>
2022-05-12 16:47:11 +0300

    monday 3 complite

--- src/p2-homeworks/h3/HW3.tsx
+++ src/p2-homeworks/h3/HW3.tsx
@@ -1,19 +1,24 @@
 import React, {useState} from 'react'
+import {v1} from 'uuid'
 import GreetingContainer from './GreetingContainer'
+
 
 // types
 export type UserType = {
-    _id: any // need to fix any
-    name: any // need to fix any
+    _id: string // need to fix any --- >string
+    name: string // need to fix any --- >string
 }
 
 // уровень работы с глобальными данными
 function HW3() {
-    const [users, setUsers] = useState<any>([]) // need to fix any
+    const [users, setUsers] = useState<Array<UserType>>([]) // need to fix any = any --->  Array<UserType>
 
-    const addUserCallback = (name: any) => { // need to fix any
-        setUsers([]) // need to fix
+    const addUserCallback = (name: string) => { // need to fix any -->string
+
+        setUsers([...users, {_id: v1(), name: name}]) // need to fix [] --> [...users, {_id:v1(), name:name}]
+
     }
+
 
     return (
         <div>
@@ -21,9 +26,13 @@ function HW3() {
             homeworks 3
 
             {/*should work (должно работать)*/}
-            <GreetingContainer users={users} addUserCallback={addUserCallback}/>
+            <GreetingContainer
+                users={users}
+                addUserCallback={addUserCallback}
+            />
 
             <hr/>
+            <button onClick={()=>console.log(users)}>?log Users?</button>
             {/*для личного творчества, могу проверить*/}
             {/*<AlternativeGreeting/>*/}
             <hr/>
