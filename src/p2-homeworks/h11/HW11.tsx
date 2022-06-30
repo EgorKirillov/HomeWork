6057e028822b3cb3a6b4fc20073ade44f8c92949
Egor <80296253508@mail.ru>
2022-06-30 19:02:06 +0300

    homework 11 need add  yarn add nouislider

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -3,8 +3,17 @@ import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState(10)
+    const [value2, setValue2] = useState(50)
+
+    const onChangeRangeHandler = (num: number) => {
+        setValue1(num)
+
+    }
+    const onChangeRangeHandler2 = (num: number) => {
+
+        setValue2((num<value1)?value1:num)
+    }
 
     return (
         <div>
@@ -14,14 +23,17 @@ function HW11() {
             {/*should work (должно работать)*/}
             <div>
                 <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
+                <SuperRange onChangeRange={onChangeRangeHandler}
+                            value={value1}
+                    // сделать так чтоб value1 изменялось+
                 />
             </div>
 
             <div>
                 <span>{value1}</span>
-                <SuperDoubleRange
+                <SuperDoubleRange onChangeRange1={onChangeRangeHandler}
+                                  onChangeRange2={onChangeRangeHandler2}
+                                  value={[value1,value2]}
                     // сделать так чтоб value1 и value2 изменялось
                 />
                 <span>{value2}</span>
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,23 +1,52 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
 
 type SuperDoubleRangePropsType = {
-    onChangeRange?: (value: [number, number]) => void
-    value?: [number, number]
+    onChangeRange1?: (value: number) => void
+    onChangeRange2?: (value: number) => void
+    value: [number, number]
     // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
+        onChangeRange1, onChangeRange2, value, ...restProps
         // min, max, step, disable, ...
     }
 ) => {
     // сделать самому, можно подключать библиотеки
+    //const finalRangeClassName = `${s.range} ${className ? className : ''}`
+
+    const onChangeCallback1 = (e: ChangeEvent<HTMLInputElement>) => {
+
+        onChangeRange1 && onChangeRange1((+e.currentTarget.value>value[1]) ? value[1]:+e.currentTarget.value)
+    }
+    const onChangeCallback2 = (e: ChangeEvent<HTMLInputElement>) => {
+        onChangeRange2 && onChangeRange2(+e.currentTarget.value)
+    }
 
     return (
+
         <>
-            DoubleRange
+            <input
+                type={'range'}
+                onChange={onChangeCallback1}
+                // className={finalRangeClassName}
+                value={(value) ? value[0] : 1}
+
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+            />
+            <input
+                type={'range'}
+                onChange={onChangeCallback2}
+                // className={finalRangeClassName}
+                value={(value) ? value[1] : 1}
+                // min={(value) ? value[0] : 1}
+                max={100}
+                {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
+            />
+
         </>
+
     )
 }
 
