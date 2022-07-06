c37ca9b9ae437ae2e664515591e77484fc3f1074
EgorKirillov <80296253508@mail.ru>
2022-07-06 14:36:29 +0300

    11 homework complite

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,13 +1,14 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import s from "./HW11.module.css"
 
 function HW11() {
     const [value1, setValue1] = useState(10)
     const [value2, setValue2] = useState(50)
 
     const onChangeRangeHandler = (num: number) => {
-        setValue1(num)
+        setValue1(num>value2?value2:num)
 
     }
     const onChangeRangeHandler2 = (num: number) => {
@@ -22,7 +23,7 @@ function HW11() {
 
             {/*should work (должно работать)*/}
             <div>
-                <span>{value1}</span>
+                <span className={s.firstSpan} >{value1}</span>
                 <SuperRange onChangeRange={onChangeRangeHandler}
                             value={value1}
                     // сделать так чтоб value1 изменялось+
@@ -30,7 +31,7 @@ function HW11() {
             </div>
 
             <div>
-                <span>{value1}</span>
+                <span className={s.secondSpan} >{value1}</span>
                 <SuperDoubleRange onChangeRange1={onChangeRangeHandler}
                                   onChangeRange2={onChangeRangeHandler2}
                                   value={[value1,value2]}
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,53 +1,54 @@
 import React, {ChangeEvent} from 'react'
+import s from "./SuperDoubleRange.module.css"
 
 type SuperDoubleRangePropsType = {
-    onChangeRange1?: (value: number) => void
-    onChangeRange2?: (value: number) => void
-    value: [number, number]
-    // min, max, step, disable, ...
+   onChangeRange1?: (value: number) => void
+   onChangeRange2?: (value: number) => void
+   value: [number, number]
+   // min, max, step, disable, ...
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
-    {
-        onChangeRange1, onChangeRange2, value, ...restProps
-        // min, max, step, disable, ...
-    }
+  {
+     onChangeRange1, onChangeRange2, value, ...restProps
+     // min, max, step, disable, ...
+  }
 ) => {
-    // сделать самому, можно подключать библиотеки
-    //const finalRangeClassName = `${s.range} ${className ? className : ''}`
-
-    const onChangeCallback1 = (e: ChangeEvent<HTMLInputElement>) => {
-
-        onChangeRange1 && onChangeRange1((+e.currentTarget.value>value[1]) ? value[1]:+e.currentTarget.value)
-    }
-    const onChangeCallback2 = (e: ChangeEvent<HTMLInputElement>) => {
-        onChangeRange2 && onChangeRange2(+e.currentTarget.value)
-    }
-
-    return (
-
-        <>
-            <input
+   // сделать самому, можно подключать библиотеки
+   //const finalRangeClassName = `${s.range} ${className ? className : ''}`
+   
+   const onChangeCallback1 = (e: ChangeEvent<HTMLInputElement>) => {
+      
+      onChangeRange1 && onChangeRange1((+e.currentTarget.value > value[1]) ? value[1] : +e.currentTarget.value)
+   }
+   const onChangeCallback2 = (e: ChangeEvent<HTMLInputElement>) => {
+      onChangeRange2 && onChangeRange2(+e.currentTarget.value)
+   }
+   
+   return (
+     
+     <div className={s.rangeslider}>
+                    <input
                 type={'range'}
                 onChange={onChangeCallback1}
-                // className={finalRangeClassName}
+                className={s.finalRangeClassName1}
                 value={(value) ? value[0] : 1}
-
+  
                 {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
-            />
-            <input
+              />
+              <input
                 type={'range'}
                 onChange={onChangeCallback2}
-                // className={finalRangeClassName}
+                 className={s.finalRangeClassName}
                 value={(value) ? value[1] : 1}
                 // min={(value) ? value[0] : 1}
                 max={100}
                 {...restProps} // отдаём инпуту остальные пропсы если они есть (value например там внутри)
-            />
-
-        </>
-
-    )
+              />
+     
+     </div>
+   
+   )
 }
 
 export default SuperDoubleRange
